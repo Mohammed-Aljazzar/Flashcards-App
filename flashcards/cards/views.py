df74a844179606f7407b223c8357a22daae3299b
Mohammed-Aljazzar <m.i.aljazzar19@gmail.com>
2025-03-06 14:26:04 +0200

    add some updates

--- flashcards/cards/views.py
+++ flashcards/cards/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView
 from .models import Card 
 from django.urls import reverse_lazy
-
+from .forms import CardCheckForm 
+import random
 # Create your views here.
 
 # https://docs.djangoproject.com/en/4.0/ref/class-based-views/
@@ -18,3 +19,27 @@ class CardCreateView(CreateView):
 
 class CardUpdateView(CardCreateView, UpdateView): 
     success_url = reverse_lazy("card-list")
+
+
+class BoxView(CardListView): 
+    template_name = "cards/box.html"
+    form_class = CardCheckForm
+    
+    def get_queryset(self): 
+        return Card.objects.filter(box=self.kwargs["box_num"]) 
+    
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs) 
+        context["box_number"] = self.kwargs["box_num"] 
+        if self.object_list: 
+            context["check_card"] = random.choice(self.object_list) 
+        return context 
+    
+    def post(self, request, *args, **kwargs): 
+        form = self.form_class(request.POST) 
+        if form.is_valid(): 
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"]) 
+            card.move(form.cleaned_data["solved"]) 
+ 
+        return redirect(request.META.get("HTTP_REFERER"))
+
